95b4db477e194e06d6330004e0f00e95951a09f1
Tales Palma <tales.palma_2022@outlook.com>
2024-07-19 21:23:17 -0300

    assets add with format

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    sprite::{MaterialMesh2dBundle, Mesh2dHandle},
+};
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
 }
 
 struct Position {
@@ -13,4 +19,29 @@ struct Position {
 struct Snake {
     segments: Vec<Position>,
     direction: Direction,
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    //Camera sjust
+    commands.spawn(Camera2dBundle::default());
+
+    //Snake head created
+    let triangulo = Mesh2dHandle(meshes.add(Triangle2d::new(
+        Vec2::Y * 50.0,
+        Vec2::new(-50.0, -50.0),
+        Vec2::new(50.0, -50.0),
+    )));
+
+    let color = Color::hsl(0.0, 1.0, 0.5);
+
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: triangulo.clone(),
+        material: materials.add(color),
+        transform: Transform::from_xyz(0.0, 0.0, 0.0),
+        ..default()
+    });
 }
