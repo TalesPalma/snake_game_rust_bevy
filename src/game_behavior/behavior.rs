3f15caf9f3fcb6b16ba4c974310f425aeb5c2726
Tales Palma <tales.palma_2022@outlook.com>
2024-07-25 20:41:25 -0300

    Att behavior snake game

--- src/game_behavior/behavior.rs
+++ src/game_behavior/behavior.rs
@@ -1,21 +1,26 @@
 use bevy::prelude::*;
 
-use crate::{
-    apple::apple_systems::*,
-    snake::{self, snake_systems::*},
-};
+use crate::{apple::apple_systems::*, snake::snake_systems::*};
 
 pub fn behavior_game_system(snake_query: Query<&Snake>, apple_query: Query<&Apple>) {
     //Se a cobra colidir com a maca
     for snake in snake_query.iter() {
         for apple in apple_query.iter() {
-            if snake.position == apple.position {
+            if is_colliding(&snake, &apple) {
                 println!("A cobra comeu a maca");
+            } else {
+                println!("A cobra nao comeu a maca ainda")
             }
         }
     }
-    //Chame a funcao de checar colisao
-    //E depois de checar a colisao , remova aquela maca e add um nova maca
-    //E adicione a maca em um position aleatoria no caso chamando a funcao de spawn radom
-    //locating
+}
+
+//Precisamos implementar um size para cobra e a maca
+fn is_colliding(snake: &Snake, apple: &Apple) -> bool {
+    let position_snake_x = snake.position.x;
+    let position_snake_y = snake.position.y;
+    let position_apple_x = apple.position.x;
+    let position_apple_y = apple.position.y;
+
+    position_apple_y == position_snake_y && position_apple_x == position_snake_x
 }
